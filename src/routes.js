24e544987b4969600199e20c2c90acaab86da4b5
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2021-08-04 19:59:22 -0300

    Standardizing the response objects

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const router = new express.Router()
 const deta = Deta('b0t6xspl_PfV5pfhncSNXq84EkMki2FtjUrXMH57R')
 const db = deta.Base('whatsapp')
 
-// Creates a new user in the database. Can accept a "key" parameter
+// Creates a new user in the database. Can accept a 'key' parameter
 router.post(['/', '/:key'], async (req, res) => {
     const obj = { key: generateId(), createdAt: new Date(), count: 0, links: [], slug: false }
 
@@ -21,17 +21,18 @@ router.post(['/', '/:key'], async (req, res) => {
     
     // Check if key is already used
     if(obj.slug && await keyExists(db, obj.key)){
-        return res.status(400).send("Ops! The key you passed is already in use.")
+        return res.status(400).send({error: 'Ops! The key you passed is already in use.'})
     }
     
     // Put the object in the database
     db.put(obj)
-      .then(() => {
-        obj.slug ?
-          res.status(200).send(`Done! The key you passed is available. Just to remind you, here it is: ${obj.key}`) :
-          res.status(200).send(`Done! Here is your key: ${obj.key}`)
-      })
-      .catch(err => res.status(400).send(err))
+        .then(() => 
+            res.status(200).send({
+                message: (obj.slug ? `Done! The key you passed is available. Just to remind you, here it is: ${obj.key}` : `Done! Here is your key: ${obj.key}`),
+                key: obj.key
+            })
+        )
+        .catch(error => res.status(400).send(error))
 })
 
 // Adds a link to the array by key
@@ -42,16 +43,16 @@ router.post('/:key/add', async (req, res) => {
     if(user){
         let link = req.query.link || req.body.link
 
-        if(!link) return res.status(400).send('Group link was not passed.')
-        if(!link.includes('whatsapp')) link = "https://chat.whatsapp.com/" + link
-        if(user.links.includes(link)) return res.status(400).send('Group link is already included.')
+        if(!link) return res.status(400).send({error: 'Group link was not passed.'})
+        if(!link.includes('whatsapp')) link = 'https://chat.whatsapp.com/' + link
+        if(user.links.includes(link)) return res.status(400).send({error: 'Group link is already included.'})
         
-        const updates = { "links": db.util.append(link) }
+        const updates = { 'links': db.util.append(link) }
 
         await db.update(updates, key)
-            .then(() => { res.status(200).send('New link added successfully!') })
-            .catch(err => res.status(400).send(err))
-    } else res.status(404).send('User not found.')
+            .then(() => res.status(200).send({message: 'New link added successfully!'}))
+            .catch(error => res.status(400).send({error}))
+    } else res.status(404).send({error: 'User not found.'})
 })
 
 // Redirects to current link by key
@@ -61,17 +62,16 @@ router.get('/:key', async (req, res) => {
     const user = await db.get(key)
 
     if(user){
-        await db.update({"count": db.util.increment(1)}, key)
+        await db.update({'count': db.util.increment(1)}, key)
             .then(() => {})
             .catch(err => res.status(400).send(err))
 
         let index = findGroupPosition(user.count + 1)
         let link = user.links[index]
 
-        redirect ? res.redirect(link) : res.status(200).send({ link })
-        
+        return redirect ? res.redirect(link) : res.status(200).send({ link })
     }
-    else res.status(404).send('User not found.')
+    else res.status(404).send({error: 'User not found.'})
 })
 
 // Returns user information by key
@@ -80,9 +80,9 @@ router.get('/:key/info', async (req, res) => {
     if(key){
         const user = await db.get(key)
         delete user.slug
-        user ? res.status(200).send({...user}) : res.status(404).send('User not found.')            
+        return user ? res.status(200).send({...user}) : res.status(404).send({error: 'User not found.'})            
     } 
-    else res.status(404).send('Key was not passed.')
+    else res.status(404).send({error: 'Key was not passed.'})
 })
 
 module.exports = router
